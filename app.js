fc01e674440d418634954a33ad07cabaffcdb048
utkarshminhas <utkarsh0304@gmail.com>
2020-10-11 23:21:09 +0530

    api works for login and signup

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var expressHbs = require('express-handlebars');
+var indexRouter = require('./routes/index');
+var userRouter = require('./routes/user');
+var mongoose = require('mongoose');
+
+var app = express();
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb+srv://Utkarsh_Kaleido:kaleidopw@cluster0.sza84.mongodb.net/KDB?retryWrites=true&w=majority')
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+// view engine setup
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/user.js
@@ -0,0 +1,123 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+var bodyParser = require('body-parser')
+/*
+// create application/json parser
+var jsonParser = bodyParser.json()
+*/
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const User = require('../models/user')
+
+router.get('/signup', function (req, res, next) {
+    res.render('signup', { title: 'Kaleido' });
+});
+
+router.post('/signup', urlencodedParser, (request, response, next) => {
+
+    // console.log("req body");
+    // console.log(request.body);
+    User.find({ email: request.body.email }).exec().then(user => {
+        if (user.length >= 1) {
+
+        } else {
+            bcrypt.hash(request.body.pass, 10, (err, hash) => {
+                if (err) {
+                    return response.status(500).json({
+                        error: err,
+                    });
+
+                } else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: request.body.email,
+                        name: request.body.name,
+                        password: hash,
+
+                    });
+
+                    console.log(user);
+
+                    user.save().then(result => {
+                        console.log("result after saving  user in KDB");
+
+                        console.log(result);
+                        response.status(201).json({
+                            message: 'User Created'
+                        });
+                    })
+                        .catch(err => {
+                            console.log(err);
+                            response.status(500).json({
+                                error: err
+                            })
+                        });
+
+                }
+                // response.render('index', { title: 'Kaleido' });
+            })
+        }
+    });
+});
+
+
+router.get('/login', function (req, res, next) {
+    res.render('login', { title: 'Kaleido' });
+});
+
+router.post('/login', urlencodedParser, (req, res, next) => {
+
+    console.log("req body");
+    console.log(req.body);
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    what: "email does not exist",
+                    message: "Auth failed"
+                });
+            }
+            bcrypt.compare(req.body.pass, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: "Auth failed"
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                        // process.env.JWT_KEY,
+                        "JWT_KEY_SECRET",
+                        {
+                            expiresIn: "1h"
+                        }
+                    );
+                    return res.status(200).json({
+                        message: "Auth successful",
+                        token: token
+                    });
+                }
+                res.status(401).json({
+                    message: "Auth failed"
+                });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+
+module.exports = router;
